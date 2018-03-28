57116b0d58c6f22c47ac078d186d0b72970118fa
David Cooley <dcooley@symbolix.com.au>
2018-03-28 13:47:38 +1100

    started code

---
+++ src/geojson_sfg.cpp
@@ -0,0 +1,26 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//#include <rapidjson/writer.h>
+//#include <rapidjson/stringbuffer.h>
+#include <rapidjson/document.h>
+
+using namespace rapidjson;
+
+/*
+ * Extract Coordinates
+ *
+ * @param coord_array rapidjson array of coordinates
+ * @param pt NumericVector in whcih to store the coordinates
+ */
+void extract_coordinates(const Value& coord_array, Rcpp::NumericVector& pt) {
+
+
+
+}
+
+
+
+
+
+
