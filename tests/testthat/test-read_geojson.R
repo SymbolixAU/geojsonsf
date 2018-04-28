114b34a695b4be5b178d12c9289c1cd3b1ccb370
symbolixau <dcooley@symbolix.com.au>
2018-04-28 18:19:13 +1000

    tests

--- tests/testthat/test-read_geojson.R
+++ tests/testthat/test-read_geojson.R
@@ -8,6 +8,17 @@ test_that("can read from various sources", {
 	url <- "http://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_outline_500k.json"
 
 	## direct from url
+	con <- curl::curl(url)
+	lines <- geojsonsf:::read_url(con)
+	close(con)
+	expect_true(nchar(lines) == 1078089)
+
+	sf <- geojson_sf(lines)
+	sfc <- geojson_sfc(lines)
+
+	expect_true(nrow(sf) == 615)
+	expect_true(length(sfc) == 615)
+
 	sf <- geojson_sf(url)
 	sfc <- geojson_sfc(url)
 
@@ -35,6 +46,15 @@ test_that("can read from various sources", {
 
 	expect_true(nrow(sf) == length(sfc))
 	expect_true(nrow(sf) == 41)
+
+	sf <- geojson_sf(system.file("examples", "geo_melbourne.geojson", package = "geojsonsf"))
+	sfc <- geojson_sfc(system.file("examples", "geo_melbourne.geojson", package = "geojsonsf"))
+
+	expect_true(nrow(sf) == length(sfc))
+	expect_true(nrow(sf) == 41)
+
+
+
 })
 
 test_that("read utils work", {
