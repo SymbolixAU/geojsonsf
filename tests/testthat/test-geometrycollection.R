8d16541a7bd9b1a33957f2263d91f2aa9c812266
David Cooley <dcooley@symbolix.com.au>
2018-05-01 12:43:07 +1000

    fixed vignette

--- tests/testthat/test-geometrycollection.R
+++ tests/testthat/test-geometrycollection.R
@@ -26,35 +26,28 @@ test_that("expand_geometriesing preserves properties, ", {
 	js <- '[{
 	"type": "Feature",
 	"properties" : {},
-	"geometry": {"type": "Polygon","coordinates": [[[-10.0, -10.0],[10.0, -10.0],[10.0, 10.0],[-10.0, -10.0]]]}
-  },{
+	"geometry": {"type": "Polygon","coordinates": [[[-10.0, -10.0],[10.0, -10.0],[10.0, 10.0],[-10.0, -10.0]]]}},{
 	"type": "Feature",
 	"properties" : {"id":1},
 	"geometry": {"type": "MultiPolygon",
 	"coordinates": [[[[180.0, 40.0], [180.0, 50.0], [170.0, 50.0],[170.0, 40.0], [180.0, 40.0]]],
-	[[[-170.0, 40.0], [-170.0, 50.0], [-180.0, 50.0],[-180.0, 40.0], [-170.0, 40.0]]]]}
-	},{
+	[[[-170.0, 40.0], [-170.0, 50.0], [-180.0, 50.0],[-180.0, 40.0], [-170.0, 40.0]]]]}},{
 	"type": "FeatureCollection",
 	"features": [{
 	"type": "Feature",
 	"properties": {"id" : 2, "value" : "foo"},
-	"geometry": {"type": "Point","coordinates": [100.0, 0.0]}
-	},{
+	"geometry": {"type": "Point","coordinates": [100.0, 0.0]}},{
 	"type": "Feature",
 	"properties": null,
 	"geometry": {
 	"type": "LineString",
-	"coordinates": [[101.0, 0.0],[102.0, 1.0]]
-	}}]
-	},{
+	"coordinates": [[101.0, 0.0],[102.0, 1.0]]}}]},{
 	"type": "GeometryCollection",
 	"geometries": [
 	  {"type": "Point","coordinates": [100.0, 0.0]},
 	  {"type": "LineString","coordinates": [[101.0, 0.0],[102.0, 1.0]]},
-	  {"type" : "MultiPoint","coordinates" : [[0,0],[1,1],[2,2]]}
-  ]},
-	{"type": "Polygon","coordinates": [[[-10.0, -10.0],[10.0, -10.0],[10.0, 10.0],[-10.0, -10.0]]]}
-	]'
+	  {"type" : "MultiPoint","coordinates" : [[0,0],[1,1],[2,2]]}]},
+	{"type": "Polygon","coordinates": [[[-10.0, -10.0],[10.0, -10.0],[10.0, 10.0],[-10.0, -10.0]]]}]'
 
 	expect_true(nrow(geojson_sf(js, expand_geometries = F)) == 6)
 	expect_true(nrow(geojson_sf(js, expand_geometries = T)) == 8)
